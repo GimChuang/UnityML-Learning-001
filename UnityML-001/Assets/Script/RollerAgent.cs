b1d1f9f8936e402115115c272aaaba62f8921a0c
gim <rgm79ms.gmail.com>
2020-06-30 23:06:53 +0800

    Initial commit

---
+++ UnityML-001/Assets/Script/RollerAgent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerAgent : Agent
+{
+    public Transform target;
+    public float speed = 10f;
+
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        if (transform.localPosition.y < 0) // If the agent falls off platform, zero its momentum
+        {
+            rigidbody.angularVelocity = Vector3.zero;
+            rigidbody.velocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 5f, 0);
+        }
+
+        // Move the target to a new location
+        target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+        // Agent velocity
+        sensor.AddObservation(rigidbody.velocity.x);
+        sensor.AddObservation(rigidbody.velocity.z);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rigidbody.AddForce(controlSignal * speed);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(transform.localPosition, target.localPosition);
+
+        // If the agent reaches the target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // If the agent falls off platform, end the episode so that it can reset itself
+        if (transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
